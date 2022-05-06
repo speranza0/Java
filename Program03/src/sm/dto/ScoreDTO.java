ac7effa7b7207cf17301bba2bf11b4d7bcd1c159
speranza0 <speranza0p@gmail.com>
2022-05-06 18:53:19 +0900

    Program03 학생 정보 완료 성적정보 삭제 버그 수정중

--- Program03/src/sm/dto/ScoreDTO.java
+++ Program03/src/sm/dto/ScoreDTO.java
@@ -1,17 +1,67 @@
 package sm.dto;
 
-public class ScoreDTO {
+public class ScoreDTO extends StudentDTO{
 	private int kor;
 	private int math;
 	private int eng;
+	private int sum;
+	private float avg;
 	
-	public ScoreDTO() {
-		
+	public ScoreDTO(int studentNum, String studentName, int kor, int math, int eng) {
+		super(studentNum, studentName);
+		this.kor = kor;
+		this.math = math;
+		this.eng = eng;
+		this.sum = kor + math + eng;
+		this.avg = sum / 3.00f;
 	}
 	
 	public ScoreDTO(int kor, int math, int eng) {
 		this.kor = kor;
 		this.math = math;
 		this.eng = eng;
+		this.sum = kor + math + eng;
+		this.avg = sum / 3.00f;
 	}
+	
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getSum() {
+		return sum;
+	}
+
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+
+	public float getAvg() {
+		return avg;
+	}
+
+	public void setAvg(float avg) {
+		this.avg = avg;
+	}
+	
 }
--- Program03/src/sm/repository/Repository.java
+++ Program03/src/sm/repository/Repository.java
@@ -1,31 +1,45 @@
 package sm.repository;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import sm.dto.ScoreDTO;
 import sm.dto.StudentDTO;
 
 public class Repository {
 	private List<StudentDTO> studentList;
+	private List<ScoreDTO> scoreList;
 	
 	public Repository() {
 		studentList = new ArrayList<StudentDTO>();
+		scoreList = new ArrayList<ScoreDTO>();
 	}
 	
-	// 등록
+	
+	// 학생 등록
 	public void createOne(StudentDTO emp) {
+		// 학번생성
 		int studentNum = (int)(Math.random()*999999 + 1);
 		emp.setStudentNum(studentNum);
+		
 		studentList.add(emp);
-		System.out.println(emp);
+		
+		// 나이 계산해서 생성
+		LocalDate date = LocalDate.now();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
+		LocalDate dateTime = LocalDate.parse(emp.getStudentBirth(), dtf);
+		int studentAge = date.compareTo(dateTime);
+		emp.setStudentAge(studentAge);
 	}
 	
-	// 전체 조회
+	// 학생 전체 조회
 	public List<StudentDTO> selectAll() {
 		return studentList;
 	}
 	
-	// 한명 조회
+	// 학생 검색
 	public StudentDTO selectOne(int idx) {
 		for(int i = 0; i < studentList.size(); i++) {
 			if(studentList.get(i) != null && studentList.get(i).getStudentNum() == idx) {
@@ -35,23 +49,70 @@ public class Repository {
 		return null;
 	}
 	
-	// 수정
+	// 학생 수정
 	public void updateOne(int idx, StudentDTO emp) {
 		for(int i = 0; i < studentList.size(); i++) {
 			if(studentList.get(i) != null && studentList.get(i).getStudentNum() == idx) {
 				emp.setStudentNum(idx);
 				studentList.remove(i);
 				studentList.add(emp);
+				
+				scoreList.get(i).setStudentName(emp.getStudentName());
+				
+				LocalDate date = LocalDate.now();
+				DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
+				LocalDate dateTime = LocalDate.parse(emp.getStudentBirth(), dtf);
+				int studentAge = date.compareTo(dateTime);
+				emp.setStudentAge(studentAge);
 				break;
 			}
 		}
 	}
 	
-	// 삭제
+	// 학생 삭제
 	public void removeOne(int idx, StudentDTO emp) {
 		for(int i = 0; i < studentList.size(); i++) {
 			if(studentList.get(i) != null && studentList.get(i).getStudentNum() == idx) {
 				studentList.remove(i);
+			}
+		}
+	}
+	
+	// 성적 등록
+	public void createScore(ScoreDTO empS) {
+		scoreList.add(empS);
+	}
+	
+	// 성적 조회
+	public ScoreDTO selectScore(int idx) {
+		for(int i = 0; i < scoreList.size(); i++) {
+			if(scoreList.get(i) != null && scoreList.get(i).getStudentNum() == idx) {
+				return scoreList.get(i);
+			}
+		}
+		return null;
+	}
+	
+	// 성적 수정
+	public void updateScore(int idx, String studentName, ScoreDTO empS) {
+		for(int i = 0; i < scoreList.size(); i++) {
+			if(scoreList.get(i) != null && scoreList.get(i).getStudentNum() == idx) {
+				empS.setStudentNum(idx);
+				empS.setStudentName(studentName);
+				scoreList.remove(i);
+				scoreList.add(empS);
+				break;
+			}
+		}
+	}
+	
+	// 성적 삭제
+	public void removeScore(int idx, String studentName, ScoreDTO empS) {
+		for(int i = 0; i < scoreList.size(); i++) {
+			if(scoreList.get(i) != null && scoreList.get(i).getStudentNum() == idx) {
+				empS.setStudentNum(idx);
+				empS.setStudentName(studentName);
+				scoreList.remove(i);
 			}
 		}
 	}
