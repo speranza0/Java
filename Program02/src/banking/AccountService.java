de99dfe47be4be35f7527cba066a573d81257b2d
speranza0 <speranza0p@gmail.com>
2022-05-13 18:21:04 +0900

    Jdbc로 MariaDB 연결해서 적용

--- Program02/src/banking/AccountService.java
+++ Program02/src/banking/AccountService.java
@@ -7,21 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AccountService {
+	AccountRepository accountRepository = new AccountRepository();
 	List<Account> accountList; 
-	// Account[] accountList;
-	
-	int accountIdx;
 	
 	static BufferedReader in;
 	
 	static {
 		in = new BufferedReader(new InputStreamReader(System.in));
-	}
-	
-	public AccountService() {
-		// accountList = new Account[100];
-		accountList = new ArrayList<Account>();
-		accountIdx = 0;
 	}
 	
 	// 메뉴 출력
@@ -53,9 +45,7 @@ public class AccountService {
 		
 		Account account = new Account(accountNum, name, balance);
 		
-		// accountList[accountIdx] = account;
-		accountList.add(account);
-		accountIdx++;
+		accountRepository.createOne(account);
 		
 		System.out.println(name + "님 계좌가 등록되었습니다.");
 	}
@@ -65,59 +55,63 @@ public class AccountService {
 		System.out.print("계좌번호 = ");
 		String accountNum = in.readLine();
 		
-		for(int i = 0; i < accountList.size(); i++) {
-			Account account = accountList.get(i);
-			if(accountNum != null && accountNum.equals(account.getAccountNum())) {
-				int input = 0;
-				int balance = account.getBalance();
-				
-				do {
-					System.out.print("입금금액 = ");
-					input = Integer.parseInt(in.readLine());
-				} while(input < 0);
-				
-				balance += input;
-				account.setBalance(balance);
-				
-				System.out.println(account.getName() + "님 현재 잔액은 " + balance + "원 입니다.");
-				break;
-			} else {
-				System.out.println("입력하신 계좌번호가 존재하지 않습니다.");
-			}
+		Account account = accountRepository.selectOne(accountNum);
+		
+		if(account == null) {
+			System.out.println("입력하신 계좌번호가 존재하지 않습니다.");
+			return;
 		}
+		
+		int input = 0;
+		int balance = account.getBalance();
+		
+		do {
+			System.out.print("입금금액 = ");
+			input = Integer.parseInt(in.readLine());
+		} while(input < 0);
+		
+		balance += input;
+		account.setBalance(balance);
+		
+		accountRepository.updateOne(account.getIdx(), account);
+		
+		System.out.println(account.getName() + "님 현재 잔액은 " + balance + "원 입니다.");
 	}
+	
 	// 출금 처리
 	public void outMoney() throws IOException {
 		System.out.print("계좌번호 = ");
 		String accountNum = in.readLine();
 		
-		for(int i = 0; i < accountList.size(); i++) {
-			Account account = accountList.get(i);
-			if(accountNum != null && accountNum.equals(account.getAccountNum())) {
-				int output = 0;
-				int balance = account.getBalance();
-				
-				do {
-					System.out.print("출금금액 = ");
-					output = Integer.parseInt(in.readLine());
-				} while(output < 0);
-				
-				if(balance - output < 0) {
-					System.out.println("잔액이 모자랍니다.");
-				} else {
-					balance -= output;
-					account.setBalance(balance);
-					System.out.println(account.getName() + "님 현재 잔액은 " + balance + "원 입니다.");
-				}
-				break;
-			} else {
-				System.out.println("입력하신 계좌번호가 존재하지 않습니다.");
-			}
+		Account account = accountRepository.selectOne(accountNum);
+		
+		if(account == null) {
+			System.out.println("입력하신 계좌번호가 존재하지 않습니다.");
+			return;
+		}
+		
+		int output = 0;
+		int balance = account.getBalance();
+		
+		do {
+			System.out.print("출금금액 = ");
+			output = Integer.parseInt(in.readLine());
+		} while(output < 0);
+		
+		if(balance - output < 0) {
+			System.out.println("잔액이 모자랍니다.");
+		} else {
+			balance -= output;
+			account.setBalance(balance);
+			accountRepository.updateOne(account.getIdx(), account);
+			System.out.println(account.getName() + "님 현재 잔액은 " + balance + "원 입니다.");
 		}
 	}
 	// 계좌 정보
 	public void showAllAccount() {
 		System.out.println("\n==== 전체 고객 조회 ====");
+		
+		List<Account> accountList = accountRepository.selectAll();
 		
 		for(int i = 0; i < accountList.size(); i++) {
 			Account account = accountList.get(i);
