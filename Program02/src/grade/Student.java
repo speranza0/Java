3fc4546a3a3c3e13ff7e679a070140aa3663e389
speranza0 <speranza0p@gmail.com>
2022-05-16 12:31:08 +0900

    jdbc로 MariaDB 연결해서 적용

--- Program02/src/grade/Student.java
+++ Program02/src/grade/Student.java
@@ -13,6 +13,15 @@ public class Student {
 	// 생성자 메서드
 	public Student() {}
 	
+	public Student(String name, int kor, int eng, int math) {
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.sum = kor + eng + math;
+		this.avg = sum / 3.0f;
+	}
+	
 	public Student(int idx, String name, int kor, int eng, int math) {
 		this.idx = idx;
 		this.name = name;
