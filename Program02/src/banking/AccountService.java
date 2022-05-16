a3a12c41962e94423812e0178cc1b70d895f7fd0
user <user@DESKTOP-P1K3VG9>
2022-05-16 12:30:47 +0900

    jdbc 유동적 url 수정

--- Program02/src/banking/AccountService.java
+++ Program02/src/banking/AccountService.java
@@ -3,7 +3,6 @@ package banking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
 public class AccountService {
